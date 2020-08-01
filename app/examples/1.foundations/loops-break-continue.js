81bfbb37ef2a111318acc51af964270fb9c52bc5
joan-garcia <joan.garcia@ceiba.com.co>
2020-08-01 16:10:47 -0500

    loops-break-continue

--- app/examples/1.foundations/loops-break-continue.js
+++ app/examples/1.foundations/loops-break-continue.js
@@ -7,33 +7,40 @@ import { log, logTitle } from "logger";
 logTitle("Break and Continue");
 /* coding examples */
 
+let name;
+let age;
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
 var persons = [
   { name: "Alex", age: 22 },
-  { name: "Maria", age: 30 }
+  { name: "Maria", age: 30 },
+  new Person("Joan", 25),
+  new Person("Angie", 22)
 ];
 
-// for (var i = 0; i <= persons.length; i++) {
-//   log(persons[i].name);
-//   log(persons[i].age);
-//   log("----------------")
-// }
+/* for (let i = 0; i < persons.length; i++) {
+  const name = persons[i].name;
+  const age = persons[i].age;
+  log(`${name} - ${age}`);
+} */
 
 var number = 0;
 
-while(number < 5) {
+while (number < 5) {
   number ++;
 
   if (number < 5) {
     continue;
   }
-
   log(number);
-
 }
 
-// var i = 0;
-
-// do {
-//   i ++;
-//   log(i)
-// } while(false);
+/* var i = 0;
+do {
+  i ++;
+  log(i);
+} while (i < 5); */
