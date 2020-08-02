9a5f6b1a6be223fad3317755bd282a2786646ba4
joan-garcia <joan.garcia@ceiba.com.co>
2020-08-02 17:07:46 -0500

    arrow functions

--- app/examples/3.syntax/arrowFunctions.js
+++ app/examples/3.syntax/arrowFunctions.js
@@ -5,21 +5,39 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('Arrow Functions');
 
+/*
+const hello = function() {
+  const es6 = 'ES6';
+  return `Hello ${es6}`;
+};
+*/
 const hello = () => {
   const es6 = 'ES6';
   return `Hello ${es6}`;
 };
 
-const powers =
-  [1,2,3,4,5].map((number, index) => Math.pow(number, index));
+/*
+const powers = [1,2,3,4,5].map(function(number, index) {
+  return Math.pow(number, index);
+});
+*/
+const powers = [1,2,3,4,5].map((number, index) => Math.pow(number, index));
 
-
+/*
+const add = function(n1, n2) {
+  return n1 + n2;
+};
+*/
 const add = (n1, n2) => n1 + n2;
 
+/*
+const milesToKm = function(miles) {
+  return miles * 1.60934;
+};
+*/
 const milesToKm = miles => miles * 1.60934;
-
 
 log(hello());
 log(powers);
 log(add(100,100));
-log(milesToKm(300));
+log(milesToKm(100));
