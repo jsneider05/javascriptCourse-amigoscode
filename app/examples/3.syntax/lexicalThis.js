a306055db9331a7de8861e8a5fdcaed9103b67dc
joan-garcia <joan.garcia@ceiba.com.co>
2020-08-02 17:48:15 -0500

    keyword this

--- app/examples/3.syntax/lexicalThis.js
+++ app/examples/3.syntax/lexicalThis.js
@@ -5,12 +5,30 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('Lexical this');
 
+/* bind this */
+/* that pattern */
+/* arrow function (best form) */
 const person = {
   name: 'Alex',
   cars: ['ferrari', 'lambo'],
-  toString: function() {
-    // log(`${this.name} has ${this.cars}`)
-    this.cars.forEach(car => {
+  /* In this case we cant use an arrow function because we lose
+      the contex of the "this" keyword, we dont know which person
+      cars we are referencing to */
+  toString: function () {
+    // log(`${this.name} has ${this.cars}`);
+
+    /* this.cars.forEach(function (car) {
+      log(`${this.name} has ${car}`);
+    }.bind(this)); */
+
+    /* const that = this;
+    this.cars.forEach(function (car) {
+      log(`${that.name} has ${car}`);
+    }); */
+
+    /* The easy way to fix this simply place an arrow function 
+        in this way we bind to the actual object of the "this" keyword */
+    this.cars.forEach( car => {
       log(`${this.name} has ${car}`);
     });
   }
