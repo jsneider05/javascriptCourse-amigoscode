c1e0b9a40e639352b4f550218a7b90d2e04eb3e3
joan-garcia <joan.garcia@ceiba.com.co>
2020-08-06 19:15:34 -0500

    classes and inheritance

--- app/examples/5.classes/classes.js
+++ app/examples/5.classes/classes.js
@@ -5,6 +5,16 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('ES6 Classes');
 
+/* Every single class has a constructor */
+/* The constructor is a simple method that gets invoke every time you create
+    an instance of a class */
+/* If we want to define properties of animal, we can do it inside of our
+    constructor, so when we create a instance of animal, we should pass this
+    properties */
+/* Static keyword allow us to create methods or properties that belong to the
+    actual class and not to each individual object that we instanciated.
+    It means that we dont have to create an instance to invoke the actual method */
+
 class Animal {
   constructor(name, age) {
     log(`${name} is an animal and was created`);
@@ -12,7 +22,7 @@ class Animal {
     this.age = age;
   }
 
-  static iAmAstaticMethod() {
+  static iAmStaticMethod() {
     log('I am a static method inside of an Animal class');
   }
 
@@ -29,20 +39,22 @@ class Animal {
   }
 
   logAge() {
-    log(`${this.name} is ${this.age} year old`);
+    log(`${this.name} is ${this.age} ${this.age > 1 ? 'years' : 'year' } old`);
   }
 }
 
+/* Clasess inheritance */
+
 class Dog extends Animal {
   constructor(name, age, breed) {
-    super(name, age)
+    super(name, age);
     this.breed = breed;
   }
 
   logBreed() {
     log(`${this.name} is a ${this.breed}`);
   }
-
+  /* Only for ilustration, dog inherite the methods too */
   logAgeFromDog() {
     super.logAge();
   }
@@ -61,6 +73,7 @@ class Cat extends Animal {
 const mike = new Dog('Mike', 4, 'Bulldog');
 mike.logBreed();
 mike.logAgeFromDog();
+mike.logAge();
 
 log('----------')
 
@@ -71,24 +84,20 @@ josh.sleep();
 josh.wakeUp();
 josh.logAge();
 
-/*
-Animal.iAmAstaticMethod();
+/* 
+Animal.iAmStaticMethod();
 
-const bobby = new Animal("bobby", 2);
+const bobby = new Animal('bobby', 2);
 bobby.eat();
-bobby.sleep();
-bobby.wakeUp();
 bobby.sleep();
 bobby.wakeUp();
 bobby.logAge();
 
-log('---------------');
+log('------------');
 
-const marshall = new Animal("marshall", 3);
+const marshall = new Animal('marshall', 3);
 marshall.eat();
 marshall.sleep();
 marshall.wakeUp();
-marshall.sleep();
-marshall.wakeUp();
 marshall.logAge();
-*/
+ */
