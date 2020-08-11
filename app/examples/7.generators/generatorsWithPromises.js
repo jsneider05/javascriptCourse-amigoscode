84e010d8efb86f917b6e6ea82b13b29711d00c1e
joan-garcia <joan.garcia@ceiba.com.co>
2020-08-11 12:43:36 -0500

    Generators and promises

--- app/examples/7.generators/generatorsWithPromises.js
+++ app/examples/7.generators/generatorsWithPromises.js
@@ -8,16 +8,34 @@ import { coroutine as co } from 'bluebird';
 
 logTitle('Generators & Promises');
 
-
+  /* Example 1: */
 const getRandomUsers = co(function* (n) {
-  const fetchRandomUsers = yield fetch(`https://randomuser.me/api/?results=${n}`)
+  const fetchRandomUsers = yield fetch(`https://randomuser.me/api/?results=${n}`);
   const data = yield fetchRandomUsers.json();
   return data;
 });
 
-getRandomUsers(10).then(randomUsers => {
-  randomUsers.results.forEach(user => {
-    const {gender, email} = user;
-    log(`${gender} - ${email}`);
-  });
-}).catch(err => log);
+getRandomUsers(5)
+  .then( randomUsers => {
+    randomUsers.results.forEach( user => {
+      const {gender, email} = user;
+      log(`${gender} - ${email}`);
+    });
+  })
+  .catch(error => log);
+
+  /* Example 2: */
+const getPosts = co(function* () {
+  const fetchPosts = yield fetch('http://jsonplaceholder.typicode.com/posts');
+  const posts = yield fetchPosts.json();
+  return posts;
+});
+
+getPosts()
+  .then(posts => {
+    posts.forEach( post => {
+      const { id, title } = post;
+      log(`PostId ${id} - Tittle: ${title}`);
+    })
+  })
+  .catch(error => log);
